991c9b4a09c8634b4b636fa1f8a2e0192868b513
Gonzalo Romero <gonzalo.romeroe@edu.uah.es>
2024-02-22 17:35:59 +0100

    prueba

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -18,6 +18,12 @@ public class ModeloDatosTest {
     public void testActualizarJugador() {
         System.out.println("Prueba de actualizarJugador");
 
+        System.setProperty("DATABASE_HOST", "jdbc:mysql://localhost");
+        System.setProperty("DATABASE_PORT", "3306");
+        System.setProperty("DATABASE_NAME", "baloncesto");
+        System.setProperty("DATABASE_USER", "usuario");
+        System.setProperty("DATABASE_PASS", "clave");
+
         String nombre = "Rudy";
         ModeloDatos instance = new ModeloDatos();
 
